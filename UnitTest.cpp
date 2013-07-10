b8e36813fe9665eac93fdf4a510b1381a33c192b
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2013-07-10 14:44:34 +0200

    unit test added

---
+++ UnitTest.cpp
@@ -0,0 +1,24 @@
+#include "PerfStat.h"
+
+
+/*  self consistency test 
+ *  (aka Unit test)
+ */
+
+
+
+#include<iostream>
+
+
+int main() {
+  int ret = 0;
+
+  std::cout << "we are " << PerfStat::isIntel() ? "on" << "not on" << " an Intel Machine" << std::endl; 
+
+
+
+
+
+  return ret;
+
+}
